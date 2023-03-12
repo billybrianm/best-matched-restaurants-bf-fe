91ba12b8e0816e9bcf6490a2b895ab6aa5ddc930
Billy Brian <billybrianm@hotmail.com>
2023-03-11 22:01:26 -0300

    simple table fetching data from API with axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [ restaurants, setRestaurants ] = useState([]);
+
+  useEffect(() => {
+    async function getRestaurants() {
+      await axios
+        .get("http://localhost:8080/restaurants")
+        
+        .then((response) => {
+          setRestaurants(response.data);
+        });
+    }
+      getRestaurants();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        Best Matched Restaurants
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Customer Rating</th>
-              <th>Distance</th>
-              <th>Price</th>
-              <th>Cuisine</th>
-            </tr>
-          </thead>
-          <tbody>
-            
-          </tbody>
-        </table>
-      </header>
+        <h1>Best Matched Restaurants</h1>
+     <table>
+       <thead>
+         <tr>
+           <th>Name</th>
+           <th>Customer Rating</th>
+           <th>Distance</th>
+           <th>Price</th>
+           <th>Cuisine</th>
+         </tr>   
+       </thead>   
+       <tbody>
+         {
+         restaurants.map( (restaurant,key) =>
+         <tr key={key}>
+             <td className='table-data'>{restaurant.name}</td>
+             <td className='table-data'>{restaurant.customerRating}</td>
+             <td className='table-data'>{restaurant.distance}</td>
+             <td className='table-data'>{restaurant.price}</td>
+             <td className='table-data'>{restaurant.cuisineId}</td>
+         </tr>
+         )
+       }
+       </tbody>
+     </table>
+      
     </div>
   );
 }
