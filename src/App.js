f6046cb1f2d1b0d23387d865b5fb2b457f5c1d56
Billy Brian <billybrianm@hotmail.com>
2023-03-12 15:10:28 -0300

    create RestaurantList component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,8 @@
-import { useEffect, useState } from 'react';
-import axios from "axios";
+import RestaurantList from './RestaurantList'
 import './App.css';
 
 function App() {
-  const [ restaurants, setRestaurants ] = useState([]);
-  const [ cuisines, setCuisines ] = useState([]);
-
-  useEffect(() => {
-    async function getCuisines() {
-      await axios
-        .get("http://localhost:8080/cuisines")
-        
-        .then((allCuisines) => {
-          console.log(allCuisines.data);
-          setCuisines(allCuisines.data);
-        });
-    }
-    async function getRestaurants() {
-      await axios
-        .get("http://localhost:8080/restaurants")
-        
-        .then((restaurants) => {
-          setRestaurants(restaurants.data);
-        });
-    }
-    getCuisines();
-    getRestaurants();
-  }, []);
-
-  return (
-    <div className="App">
-        <h1>Best Matched Restaurants</h1>
-        <input type="text" name="" placeholder="Name"/>
-        <input type="text" name="" placeholder="Customer Rating"/> 
-        <input type="text" name="" placeholder="Distance"/> 
-        <input type="text" name="" placeholder="Price"/> 
-        <input type="text" name="" placeholder="Cuisine"/>
-        <button>Search</button>
-     <table>
-       <thead>
-         <tr>
-           <th>Name</th>
-           <th>Customer Rating</th>
-           <th>Distance</th>
-           <th>Price</th>
-           <th>Cuisine</th>
-         </tr>   
-       </thead>   
-       <tbody>
-         {
-         restaurants.map( (restaurant,key) =>
-         <tr key={key}>
-             <td className='table-data'>{restaurant.name}</td>
-             <td className='table-data'>{restaurant.customerRating}</td>
-             <td className='table-data'>{restaurant.distance}</td>
-             <td className='table-data'>{restaurant.price}</td>
-             <td className='table-data'>{cuisines.find(cuisine => cuisine.id === restaurant.cuisineId).name}</td>
-         </tr>
-         )
-       }
-       </tbody>
-     </table>
-      
-    </div>
-  );
+  return <RestaurantList /> ;
 }
 
 export default App;
---
+++ src/RestaurantList.js
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react';
+import axios from "axios";
+
+export default function RestaurantList() {
+    const [ cuisines, setCuisines ] = useState([]);
+    const [ restaurants, setRestaurants ] = useState([]);
+
+    const [name, setName] = useState('');
+    const [customerRating, setCustomerRating] = useState('');
+    const [price, setPrice] = useState('');
+    const [distance, setDistance] = useState('');
+    const [cuisineId, setCuisineId] = useState('');
+
+    useEffect(() => {
+        async function getCuisines() {
+        await axios
+            .get("http://localhost:8080/cuisines")
+            
+            .then((allCuisines) => {
+            console.log(allCuisines.data);
+            setCuisines(allCuisines.data);
+            });
+        }    
+        getCuisines();
+    }, []);
+
+    async function getRestaurants() {
+        await axios.get(createRestaurantsUrl()).then((restaurants) => {
+            setRestaurants(restaurants.data);
+        });
+    }
+
+    const handleChange = (event) => {
+        setName(event.target.value);
+    }
+
+    function createRestaurantsUrl() {
+        let params = "?";
+        if(name)
+        params += "name=" + name;
+        if(customerRating)
+        params += "customerRating=" + customerRating;
+        if(distance)
+        params += "distance=" + distance;
+        if(price)
+        params += "price=" + price;
+        if(cuisineId)
+        params += "cuisineId=" + cuisineId;
+        
+        return "http://localhost:8080/restaurants" + params;
+    }
+
+    return (
+        <div className="App">
+            <h1>Best Matched Restaurants</h1>
+            <input type="text" name="" placeholder="Name" value={name} onChange={handleChange}/>
+            <input type="text" name="" placeholder="Customer Rating"/> 
+            <input type="text" name="" placeholder="Distance"/> 
+            <input type="text" name="" placeholder="Price"/> 
+            <input type="text" name="" placeholder="Cuisine"/>
+            <button onClick={getRestaurants}>Search</button>
+         <table>
+           <thead>
+             <tr>
+               <th>Name</th>
+               <th>Customer Rating</th>
+               <th>Distance</th>
+               <th>Price</th>
+               <th>Cuisine</th>
+             </tr>   
+           </thead>   
+           <tbody>
+             {
+             restaurants.map( (restaurant,key) =>
+             <tr key={key}>
+                 <td className='table-data'>{restaurant.name}</td>
+                 <td className='table-data'>{restaurant.customerRating}</td>
+                 <td className='table-data'>{restaurant.distance}</td>
+                 <td className='table-data'>{restaurant.price}</td>
+                 <td className='table-data'>{cuisines.find(cuisine => cuisine.id === restaurant.cuisineId).name}</td>
+             </tr>
+             )
+           }
+           </tbody>
+         </table>
+          
+        </div>
+      );
+}
