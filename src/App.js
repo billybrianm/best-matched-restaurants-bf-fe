18c70547165f187dea3dfdfd7f42126f0b94573f
Billy Brian <billybrianm@hotmail.com>
2023-03-11 21:36:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Test
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Customer Rating</th>
+              <th>Distance</th>
+              <th>Price</th>
+              <th>Cuisine</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+          </tbody>
+        </table>
       </header>
     </div>
   );
